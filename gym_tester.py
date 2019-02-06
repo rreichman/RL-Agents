2ea7d6206b3d715ff819e786dc6e82e07e2b137f
ranreichman <ran.reichman@snip.today>
2019-02-05 21:41:33 -0800

    Now have a simple and robust testing environment

--- gym_tester.py
+++ gym_tester.py
@@ -1,8 +1,41 @@
 # Play around with OpenAI Gym
 
+import gym
+
 from time import sleep
 
-import gym
-env = gym.make('CartPole-v0')
-env.reset()
-env.render()
+# An agent that operates randomly. From https://github.com/openai/gym/blob/master/examples/agents/random_agent.py
+class RandomAgent(object):
+    """The world's simplest agent!"""
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def act(self, observation, reward, done):
+        return self.action_space.sample()
+
+class GymTester(object):
+    def __init__(self, environment_type):
+        self.environment_type = environment_type
+        self.env = gym.make(self.environment_type)
+        self.env.seed(0)
+
+    def run(self, agent, episode_count):
+        ob = self.env.reset()
+
+        reward = 0
+        done = False
+
+        for i in range(episode_count):
+            self.env.render()
+            while True:
+                action = agent.act(ob, reward, done)
+                ob, reward, done, _ = self.env.step(action)
+                if done:
+                    break
+            print("Episode " + str(i) + ". Reward: " + str(reward))
+
+
+if __name__ == '__main__':
+    gym_tester = GymTester('CartPole-v0')
+    agent = RandomAgent(gym_tester.env.action_space)
+    gym_tester.run(agent, 100)
