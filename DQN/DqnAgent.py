77549404b30da740a066aa98cfe40c49bb4ca6ff
ranreichman <ran.reichman@snip.today>
2019-02-08 23:59:12 -0800

    After many experiments, code now learns successfully. Now will play around with the target network.

--- DQN/DqnAgent.py
+++ DQN/DqnAgent.py
@@ -14,7 +14,6 @@ sys.path.append(parent_directory)
 from gym_tester import *
 from utils import *
 
-# TODO: move this to a file of its own
 # The various parameters of the DQN process
 class DqnParameters(object):
     def __init__(
@@ -39,7 +38,6 @@ class DqnParameters(object):
         # The number of q-function model parameter udpates between each target network update.
         self.frequency_of_target_network_syncs = frequency_of_target_network_syncs
 
-# An agent that operates in a general environment using the DQN algorithm
 class DqnAgent(object):
     # observation_space is what the agent sees from the environment.
     # action_space is the possible actions for the agent.
@@ -53,11 +51,11 @@ class DqnAgent(object):
         self.observation_space = observation_space
         self.action_space = action_space
         self.model = self.get_new_model(observation_space, action_space, self.dqn_parameters.learning_rate)
-        self.target_model = self.get_new_model(observation_space, action_space, self.dqn_parameters.learning_rate)
-        self.model_updates_since_last_target_network_sync = 0
+        #self.target_model = self.get_new_model(observation_space, action_space, self.dqn_parameters.learning_rate)
+        #self.model_updates_since_last_target_network_sync = 0
         
         # Initialize both the target and the model network to be the same. They will sync every X steps.
-        copy_weights_from_one_nn_to_other(self.model, self.target_model)
+        #copy_weights_from_one_nn_to_other(self.model, self.target_model)
         
         self.experience_replay_memory = deque(maxlen=self.dqn_parameters.replay_memory_capacity)
 
@@ -65,7 +63,7 @@ class DqnAgent(object):
         return DqnDlModel(observation_space.shape[0], action_space.n, learning_rate)
 
     # Operates the agent in the environment
-    def act(self, state, reward, done):
+    def act(self, state):
         # Random action in case the epsilon probability of random was chosen
         action = self.action_space.sample()
         
@@ -76,12 +74,12 @@ class DqnAgent(object):
         return action
 
     # Increases the number of parameter updates by one. Then updates the target network if we've reached the threshold.
-    def sync_target_network_if_necessary(self):
+    '''def sync_target_network_if_necessary(self):
         self.model_updates_since_last_target_network_sync += 1
         if self.model_updates_since_last_target_network_sync % self.dqn_parameters.frequency_of_target_network_syncs == 0:
             print("Updated Target Network")
             copy_weights_from_one_nn_to_other(self.model, self.target_model)
-            self.model_updates_since_last_target_network_sync = 0
+            self.model_updates_since_last_target_network_sync = 0'''
 
     # Saves the recent action to experience replay
     def save_to_experience_replay(self, state, action, reward, state_next, done):
@@ -91,7 +89,6 @@ class DqnAgent(object):
         if len(self.experience_replay_memory) >= self.dqn_parameters.replay_memory_minibatch_size:
             minibatch = random.sample(self.experience_replay_memory, self.dqn_parameters.replay_memory_minibatch_size)
             for state, action, reward, state_next, done in minibatch:
-                # TODO: something is wrong here
                 q_update = reward
                 if not done:
                     q_update = reward + self.dqn_parameters.gamma * np.amax(predict(self.model, state_next)[0])
@@ -100,18 +97,17 @@ class DqnAgent(object):
                 
                 model_feed_dict = {self.model.input_layer: state, self.model.output_res: q_values}
                 self.model.fit(model_feed_dict)
-                self.sync_target_network_if_necessary()
+                #self.sync_target_network_if_necessary()
             
-        #epsilon_after_step = self.dqn_parameters.epsilon - self.dqn_parameters.epsilon_step_size
-        #self.dqn_parameters.epsilon = max(self.dqn_parameters.epsilon_min, epsilon_after_step)
-        # TODO: fix this
-        self.dqn_parameters.epsilon = max(self.dqn_parameters.epsilon_min, self.dqn_parameters.epsilon * 0.995)
+        epsilon_after_step = self.dqn_parameters.epsilon - self.dqn_parameters.epsilon_step_size
+        self.dqn_parameters.epsilon = max(self.dqn_parameters.epsilon_min, epsilon_after_step)
+        # I also tested exponential decay of epsilon. Both work.
+        #self.dqn_parameters.epsilon = max(self.dqn_parameters.epsilon_min, self.dqn_parameters.epsilon * 0.995)
 
     def close_sessions(self):
         self.model.session.close()
-        self.target_model.session.close()
+        #self.target_model.session.close()
 
-# TODO: move this to a file of its own.
 # The NN model that is used in the DQN
 class DqnDlModel(object):
     def __init__(self, environment_input_size, action_size, learning_rate):
@@ -143,11 +139,10 @@ class DqnDlModel(object):
     def fit(self, model_feed_dict):
         self.session.run(self.train_function, feed_dict=model_feed_dict)
 
-# TODO: perhaps clip changes to increase stability
+# FUTURE: perhaps clip changes to increase stability
 if __name__ == '__main__':
     gym_tester = GymTester('CartPole-v1')
 
     agent = DqnAgent(gym_tester.env.observation_space, gym_tester.env.action_space)
-    #agent = RandomAgent(gym_tester.env.action_space)
     gym_tester.run(agent, 1000)
     agent.close_sessions()
--- gym_tester.py
+++ gym_tester.py
@@ -15,7 +15,7 @@ class RandomAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
 
-    def act(self, observation, reward, done):
+    def act(self, observation, reward):
         return self.action_space.sample()
 
     def incorporate_feedback_from_action(self, state, action, reward, state_next, done):
@@ -25,41 +25,33 @@ class GymTester(object):
     def __init__(self, environment_type):
         self.environment_type = environment_type
         self.env = gym.make(self.environment_type)
-        #self.env.seed(0)
 
     def run(self, agent, episode_count):
-        reward = 0
-
-        recent_average = 0
+        observation_space_size = self.env.observation_space.shape[0]
+        dqn_agent = agent
 
         for i in range(episode_count):
             state = self.env.reset()
-            observation_space = self.env.observation_space.shape[0]
-            state = np.reshape(state, [1, observation_space])
+            state = np.reshape(state, [1, observation_space_size])
             #self.env.render()
             total_reward = 0
+
             while True:
-                action = agent.act(state, reward, done)
+                action = dqn_agent.act(state)
                 state_next, reward, done, _ = self.env.step(action)
                 reward = reward if not done else -reward
-                state_next = np.reshape(state, [1, observation_space])
+                state_next = np.reshape(state_next, [1, observation_space_size])
 
-                # TODO: When using in Atari games should use past four observations
-                agent.save_to_experience_replay(state, action, reward, state_next, done)
+                # FUTURE: When using in Atari games should use past four observations
+                dqn_agent.save_to_experience_replay(state, action, reward, state_next, done)
                 state = state_next
-                #total_reward += reward
                 total_reward += 1
-                if done:
-                    recent_average += total_reward
-                    average_stat_frequency = 10
-                    print("Run: " + str(i) + ". Reward: " + str(total_reward))
-                    print("Epsilon is: " + str(agent.dqn_parameters.epsilon))
-                    if i % average_stat_frequency == 0 and i != 0:
-                        #print("i is " + str(i) + ". Recent average: " + str(recent_average * 1.0 / average_stat_frequency))
-                        recent_average = 0
-                    break
-                agent.run_experience_replay()
 
+                if done:
+                    print("Run: " + str(i) + ". Reward: " + str(total_reward))
+                    break
+
+                dqn_agent.run_experience_replay()
 
 if __name__ == '__main__':
     gym_tester = GymTester('CartPole-v1')
